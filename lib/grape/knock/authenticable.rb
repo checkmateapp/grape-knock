428d212c855c8a493d7098e3ea934d614f876182
Luke Ivers <lukeivers@gmail.com>
2017-02-16 15:22:41 -0800

    adds some debugging to test

--- lib/grape/knock/authenticable.rb
+++ lib/grape/knock/authenticable.rb
@@ -22,7 +22,9 @@ module Grape
 
       def authenticate
         fail ::Grape::Knock::ForbiddenError unless token
+        Rails.logger.info request.inspect
         request = Grape::Request.new(env)
+        Rails.logger.info request.inspect
         @current_user = ::Knock::AuthToken.new(token: token, request: request).current_user
         fail ::Grape::Knock::ForbiddenError unless @current_user
       rescue ::JWT::DecodeError
